29542152938fe8b052bc9df3669ebc90b797e329
Egor Pogadaev <pogadaev.e@skbkontur.ru>
2020-11-30 11:44:58 +0500

    build: use BROWSER_ENV variable

--- packages/react-ui-validations/src/ValidationContainer.tsx
+++ packages/react-ui-validations/src/ValidationContainer.tsx
@@ -20,7 +20,7 @@ export class ValidationContainer extends React.Component<ValidationContainerProp
   public static __KONTUR_REACT_UI__ = 'ValidationContainer';
 
   public static defaultProps = {
-    disableSmoothScroll: process.env.NODE_ENV === 'test'
+    disableSmoothScroll: process.env.BROWSER_ENV === 'test' || process.env.NODE_ENV === 'test',
   };
 
   public static propTypes = {
--- packages/react-ui/lib/currentEnvironment.ts
+++ packages/react-ui/lib/currentEnvironment.ts
@@ -1,3 +1,4 @@
-export const isTestEnv: boolean = process.env.NODE_ENV === 'test' || Boolean(process.env.enableReactTesting);
-export const isProductionEnv: boolean = process.env.NODE_ENV === 'production';
-export const isDevelopmentEnv: boolean = process.env.NODE_ENV === 'development';
+const { BROWSER_ENV, NODE_ENV, enableReactTesting } = process.env;
+export const isTestEnv: boolean = BROWSER_ENV === 'test' || NODE_ENV === 'test' || Boolean(enableReactTesting);
+export const isProductionEnv: boolean = BROWSER_ENV === 'production' || NODE_ENV === 'production';
+export const isDevelopmentEnv: boolean = BROWSER_ENV === 'development' || NODE_ENV === 'development';
